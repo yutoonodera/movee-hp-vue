9c5a315e79a5fa3800f997e3a7b1816e2a0550e9
onody <onoderayuto@onody00.local>
2026-02-17 22:56:56 +0900

    fix

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <div class="p-6 rounded-xl shadow">
-      <h1 class="text-2xl font-bold">Nuxt + Tailwind + Docker</h1>
-      <p class="mt-2 text-gray-600">It works.</p>
+      <h1 class="text-2xl font-bold">小売業向け、マップ中心の顧客分析ソフトウェア</h1>
+      <p>主に会員データを持つ小売業様向けソフトウェアになります。顧客データを分析し、意思決定を支援します。</p>
+      <ul class="list-disc pl-6 text-gray-700 space-y-2">
+        <li>ポリゴンなどを使った商圏の見える化</li>
+        <li>顧客属性の把握</li>
+        <li>顧客生涯価値など、小売業の分析指標の見える化</li>
+      </ul>
+      <h3 class="mt-2 text-gray-600">株式会社movee</h3>
     </div>
   </div>
 </template>
+<script>
+</script>
